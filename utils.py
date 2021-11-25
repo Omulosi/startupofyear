4bd4a1ba37cd7028ba508c2f1c52a25b40b4345d
Omulosi <mulongojohnpaul@gmail.com>
2021-11-25 14:10:01 +0300

    cleanup

--- utils.py
+++ utils.py
@@ -5,13 +5,6 @@ import requests
 
 USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36'
 
-def split_name(name):
-    # split name into first name and last name
-    name = name.split()
-    if len(name) > 1:
-        return name[0], name[-1]
-    return ' '.join(name)
-
 def download(url, num_retries=2, user_agent=USER_AGENT, proxies=None):
     print('Downloading:', url)
     headers = {'User-Agent': user_agent}
@@ -41,10 +34,6 @@ def get_robots_parser(robots_url):
 def get_links(html):
     webpage_regex = re.compile("""<a[^>]+href=["'](.*?)["']""", re.IGNORECASE)
     return webpage_regex.findall(html)
-
-
-def jsonify(list1, list2):
-    return {k:v for k,v in list(zip(list1, list2))}
 
 def remove_html_tags(text):
     html_tags = re.compile('<.*?>')
